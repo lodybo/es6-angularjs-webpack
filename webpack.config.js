dc3edd14264a8d9544af81bd1560070be26ffef5
Lody Borgers <hi@lodybo.nl>
2017-08-17 01:20:51 +0200

    Created Angular app

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,31 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    'app': 'src/js/app.js',
-    'vendor': 'src/js/vendor'
+    'app': './src/js/app.js',
+    'vendors': './src/js/vendors.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9001
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'ES6-AngularJS-Webpack'
+      title: 'ES6-AngularJS-Webpack',
+      template: 'src/views/index.html'
     })
   ],
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: [ {
+        loader: 'html-loader',
+        options: {
+          minimize: false
+        }
+      }],
+    }]
+  },
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
